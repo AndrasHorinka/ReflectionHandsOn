2c1f39593aa565fa88236641d597432f2c6f9294
AndrasHorinka <horinka.andras@gmail.com>
2019-04-30 08:49:06 +0200

    first 2 handson pt2

---
+++ AssemblyAttrDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ourAssembly = Assembly.GetExecutingAssembly();
+            var type = typeof(AssemblyDescriptionAttribute);
+
+            var objects = ourAssembly.GetCustomAttributes(type, false);
+
+            if (objects.Length > 0)
+            {
+                var newObject = (AssemblyDescriptionAttribute) objects[0];
+                Console.WriteLine($"{newObject.Description}");
+            }
+        }
+    }
+}
