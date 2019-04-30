092acc56f109ef6488e2d7fc65d86db277cf4c07
AndrasHorinka <horinka.andras@gmail.com>
2019-04-30 10:54:37 +0200

    DynamicAssembly *CodeDynamic* end

--- DynamicAssembly/Program.cs
+++ DynamicAssembly/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace DynamicAssembly
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace DynamicAssembly
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var myAssembly = new AssemblyName("MyAssemblyName");
+            myAssembly.Version = new Version("88.77.66.55");
+
+            var AssBuilder =
+                AppDomain.CurrentDomain.DefineDynamicAssembly(myAssembly, AssemblyBuilderAccess.ReflectionOnly);
+
+            var MyBadB = AssBuilder.DefineDynamicModule("CodeModule", "DemoAssembly.dll");
+
+            var MyTypeB = MyBadB.DefineType("Beast", TypeAttributes.Public);
+
+            var newMyType = MyTypeB.CreateType();
+            Console.WriteLine(newMyType.FullName);
+            Console.WriteLine("---------------------");
+
+            foreach (var member in newMyType.GetMembers()) Console.WriteLine($"{member.MemberType} - {member.Name}");
         }
     }
 }
